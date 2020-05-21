451d1ded52e1f2111acfe8265ad01dcde74aec05
Alshoja <alshoja@gmail.com>
2020-05-21 12:58:44 +0530

    lib config

--- projects/break-point-observer/src/lib/break-point-observer.module.ts
+++ projects/break-point-observer/src/lib/break-point-observer.module.ts
@@ -2,8 +2,14 @@ import { BreakPointObserverService } from './break-point-observer.service';
 import { NgModule } from '@angular/core';
 import { BreakPointObserverComponent } from './break-point-observer.component';
 
+import { InjectionToken } from '@angular/core';
 
+export interface LibConfig {
+  size: string;
+  value: string;
+}
 
+export const LibConfigService = new InjectionToken<LibConfig>('LibConfig');
 @NgModule({
   declarations: [BreakPointObserverComponent],
   imports: [
@@ -11,4 +17,16 @@ import { BreakPointObserverComponent } from './break-point-observer.component';
   providers: [BreakPointObserverService],
   exports: [BreakPointObserverComponent]
 })
-export class BreakPointObserverModule { }
+export class BreakPointObserverModule {
+  static forRoot(config: LibConfig): BreakPointObserverModule {
+    return {
+      ngModule: BreakPointObserverModule,
+      providers: [
+        {
+          provide: LibConfigService,
+          useValue: config
+        }
+      ]
+    };
+  }
+}
--- projects/break-point-observer/src/lib/break-point-observer.service.ts
+++ projects/break-point-observer/src/lib/break-point-observer.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { fromEvent, Observable } from 'rxjs';
 import { startWith, map, distinctUntilChanged, shareReplay } from 'rxjs/operators';
 
-const QUERY: Map<string, string> = new Map([
+const MEDIA_QUERY: Map<string, string> = new Map([
   ['xl', '(min-width: 1200px)'],
   ['lg', '(min-width: 992px)'],
   ['md', '(min-width: 768px)'],
@@ -14,26 +14,26 @@ const QUERY: Map<string, string> = new Map([
   providedIn: 'root'
 })
 export class BreakPointObserverService {
-  private _size$: Observable<string>;
+  private screensize: Observable<string>;
 
   constructor() {
-    this._size$ = fromEvent(window, 'resize')
+    this.screensize = fromEvent(window, 'resize')
       .pipe(
-        startWith(this._getScreenSize()),
+        startWith(this.getScreenSize()),
         map((event: Event) => {
-          return this._getScreenSize();
+          return this.getScreenSize();
         }),
         distinctUntilChanged(),
         shareReplay(1)
       );
   }
 
-  public get size$(): Observable<string> {
-    return this._size$;
+  public get size(): Observable<string> {
+    return this.screensize;
   }
 
-  private _getScreenSize(): string {
-    const [[newSize = 'never']] = Array.from(QUERY.entries())
+  private getScreenSize(): string {
+    const [[newSize = 'never']] = Array.from(MEDIA_QUERY.entries())
       .filter(([size, mediaQuery]) => window.matchMedia(mediaQuery).matches);
     return newSize;
   }
